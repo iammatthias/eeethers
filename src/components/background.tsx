fe490e1bafa2e58c663dafdba8c4377e3a83827f
iammatthias <me@iammatthias.com>
2022-05-03 23:23:17 -0700

    fix: styles and more

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -83,8 +83,8 @@ export default function Background() {
           xmlns="http://www.w3.org/2000/svg"
           // viewBox={`0 0 ${width ? width : 400} ${height ? height : 400}`}
           viewBox="0 0 400 400"
-          width="100%"
-          height="100%"
+          width="100vw"
+          height="100vh"
           css={{
             zIndex: `-2`,
             position: `absolute`,
