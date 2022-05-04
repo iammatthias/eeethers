05d58de48678f6b889e1c6ff7bb50eacd929e947
iammatthias <me@iammatthias.com>
2022-05-04 14:23:16 -0700

    fix: mobile vh

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: any) {
       css={{
         margin: 0,
         padding: `0`,
-        height: `100vh`,
+        height: `calc(100vh - env(safe-area-inset-bottom))`,
         width: `100vw`,
       }}
     >
